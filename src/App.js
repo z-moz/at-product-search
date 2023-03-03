c903413c27d2c3dbc069e8df24b5767cca47eca0
z-moz <100994664+z-moz@users.noreply.github.com>
2023-03-03 15:33:25 +0000

    front end template

--- src/App.js
+++ src/App.js
@@ -1,5 +1,43 @@
 function App() {
-  return null;
+  return (
+    <body>
+      Product search
+      <form>
+        Title
+        <input></input>
+        <button type="submit">Search</button>
+      </form>
+      <table>
+        <tr>
+          <th>Image</th>
+          <th>Title</th>
+          <th>Destination</th>
+        </tr>
+        <tr>
+          <td>
+            <img
+              src="https:\/\/global.atdtravel.com\/sites\/default\/files\/imagecache\/atd_list_thumb\/ticket_description\/dubai_airport_to_city_hotel\/aircraft-2285794.jpg"
+              alt="Ajman, Sharjah and Jebel Ali Area Hotels to Dubai Airport"
+              loading="lazy"
+            ></img>
+          </td>
+          <td>Ajman, Sharjah and Jebel Ali Area Hotels to Dubai Airport</td>
+          <td>Dubai</td>
+        </tr>
+        <tr>
+          <td>
+            <img
+              src="https:\/\/global.atdtravel.com\/sites\/default\/files\/imagecache\/atd_list_thumb\/ticket_description\/dubai_airport_to_city_hotel\/aircraft-2285794.jpg"
+              alt="Ajman, Sharjah and Jebel Ali Area Hotels to Dubai Airport"
+              loading="lazy"
+            ></img>
+          </td>
+          <td>Ajman, Sharjah and Jebel Ali Area Hotels to Dubai Airport</td>
+          <td>Dubai</td>
+        </tr>
+      </table>
+    </body>
+  );
 }
 
 export default App;
