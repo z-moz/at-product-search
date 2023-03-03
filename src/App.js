316edfcd9871e3a39c80ce36ea858b72b49ea4d4
z-moz <100994664+z-moz@users.noreply.github.com>
2023-03-03 16:58:00 +0000

    data from db displays on front end

--- src/App.js
+++ src/App.js
@@ -1,6 +1,42 @@
+import React, { useState, useEffect } from "react";
+
+const BASE_URL = "https://global.atdtravel.com/api/";
+
 function App() {
+  const searchInit = {
+    geo: "en",
+    // title: "",
+    // limit: 10,
+    // offset: 0,
+    // id: "",
+  };
+
+  const [list, setList] = useState([]);
+  const [search, setSearch] = useState(searchInit);
+
+  async function searchDatabase(searchArgs) {
+    let query = new URLSearchParams(searchArgs).toString();
+    try {
+      let response = await fetch(BASE_URL + "products?" + query, {
+        method: "GET",
+      });
+      if (response.ok) return response.json();
+      throw new Error("Products not found.");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    async function getList() {
+      const result = await searchDatabase(search);
+      setList([...result.data]);
+    }
+    getList();
+  }, [search]);
+
   return (
-    <body>
+    <>
       Product search
       <form>
         Title
@@ -8,35 +44,28 @@ function App() {
         <button type="submit">Search</button>
       </form>
       <table>
-        <tr>
-          <th>Image</th>
-          <th>Title</th>
-          <th>Destination</th>
-        </tr>
-        <tr>
-          <td>
-            <img
-              src="https:\/\/global.atdtravel.com\/sites\/default\/files\/imagecache\/atd_list_thumb\/ticket_description\/dubai_airport_to_city_hotel\/aircraft-2285794.jpg"
-              alt="Ajman, Sharjah and Jebel Ali Area Hotels to Dubai Airport"
-              loading="lazy"
-            ></img>
-          </td>
-          <td>Ajman, Sharjah and Jebel Ali Area Hotels to Dubai Airport</td>
-          <td>Dubai</td>
-        </tr>
-        <tr>
-          <td>
-            <img
-              src="https:\/\/global.atdtravel.com\/sites\/default\/files\/imagecache\/atd_list_thumb\/ticket_description\/dubai_airport_to_city_hotel\/aircraft-2285794.jpg"
-              alt="Ajman, Sharjah and Jebel Ali Area Hotels to Dubai Airport"
-              loading="lazy"
-            ></img>
-          </td>
-          <td>Ajman, Sharjah and Jebel Ali Area Hotels to Dubai Airport</td>
-          <td>Dubai</td>
-        </tr>
+        <thead>
+          <tr>
+            <th>Image</th>
+            <th>Title</th>
+            <th>Destination</th>
+          </tr>
+        </thead>
+        <tbody>
+          {list.map((item) => {
+            return (
+              <tr key={item.id}>
+                <td>
+                  <img src={item.img_sml} alt={item.title} loading="lazy"></img>
+                </td>
+                <td>{item.title}</td>
+                <td>{item.dest}</td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
-    </body>
+    </>
   );
 }
 
